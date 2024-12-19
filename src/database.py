252b807a1b30e47114e43091506e634f7b027e8f
algrit <algrigorenko91@gmail.com>
2024-12-19 17:46:11 +0300

    Добавлена модель "Отели"

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -9,10 +9,5 @@ async_engine = create_async_engine(url=settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
-async def func():
-	async with async_session_maker() as conn:
-		query = text("SELECT version()")
-		res = await conn.execute(query)
-		print(res.fetchone())
-
-asyncio.run(func())
+class Base(DeclarativeBase):
+	pass
