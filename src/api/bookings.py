1527858fbebea3b08c3cd2b7e4585a9b933597b1
algrit <algrigorenko91@gmail.com>
2024-12-27 10:25:33 +0300

    Задание №12: Ручка для добавления бронирования

---
+++ src/api/bookings.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+from src.api.dependencies import DBDep, UserIdDep
+from src.schemas.bookings import BookingAddRequest, BookingAdd
+
+router = APIRouter(prefix="/bookings", tags=["Бронирования"])
+
+
+@router.post("", summary="Забронировать номер в отеле")
+async def booking_add(db: DBDep,
+                      user_id: UserIdDep,
+                      data: BookingAddRequest):
+    room = await db.rooms.get_one(id=data.room_id)
+    booking = BookingAdd(user_id=user_id, price=room.price, **data.model_dump())
+    added_booking = await db.bookings.add_one(booking)
+    await db.commit()
+    return {"status": "OK", "data": added_booking}
---
+++ src/schemas/bookings.py
@@ -0,0 +1,18 @@
+from datetime import date
+from typing import Annotated
+from pydantic import BaseModel
+
+
+class BookingAddRequest(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+class BookingAdd(BookingAddRequest):
+    user_id: int
+    price: int
+
+
+class Booking(BookingAdd):
+    id: int
